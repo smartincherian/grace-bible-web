68cd58e42e8b5beb7494c4aa0a6df381b5feca77
smartinkcherian <smartincherian@7thpillar.com>
2024-09-24 09:54:05 +0530

    Language from url

--- src/pages/Verses/index.jsx
+++ src/pages/Verses/index.jsx
@@ -1,17 +1,30 @@
 // src/Counter.js
 
 import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import ListItemsWrapper from "../../section/ListItemsWrapper";
 import { useGetVersesMutation } from "../../store/verses/service";
+import { setLocalStorage } from "../../utils/localStorage";
+import { setCurrentLanguage } from "../../store/localization/slice";
+import { useDispatch } from "react-redux";
 
 const Verses = () => {
   const { id } = useParams();
+  const [searchParams] = useSearchParams();
+  const language = searchParams.get("lang");
+  const dispatch = useDispatch();
   const [getVerses, { isLoading }] = useGetVersesMutation();
 
   useEffect(() => {
     getVersesHandler();
   }, []);
+
+  useEffect(() => {
+    if (language) {
+      setLocalStorage("language", language);
+      dispatch(setCurrentLanguage(language));
+    }
+  }, [language]);
 
   const getVersesHandler = async () => {
     try {
